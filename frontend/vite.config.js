c973176ac11b4a610104236be25316da200fff04
Dinesh Kodali <64801249+dineshkodali@users.noreply.github.com>
2025-11-28 14:44:56 +0000

    added ghpages conection vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,10 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+
+  // REQUIRED for GitHub Pages
+  base: "/<repo-name>/",
+  
   server: {
     port: 5173,
     proxy: {
